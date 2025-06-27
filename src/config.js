0bb9c4bf1bc8cd07afbc95db3326e5ca0156b703
QC3284 <133119288+QC3284@users.noreply.github.com>
2025-06-27 18:57:38 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // DDNSPOD的secretId和secretKey 获取地址: https://console.dnspod.cn/account/token/apikey
-  secretId: "",
-  secretKey: "",
+  // 阿里云的AccessKey ID和AccessKey Secret
+  accessKeyId: "",
+  accessKeySecret: "",
   // 主域名和需要DDNS的子域名
   Domain: "vvhan.com",
   SubDomain: "cf.cname"
